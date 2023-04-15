6d77ca280a95d1afcbfd8039507affce17f3ca91
kl4rk <w.lickefett@googlemail.com>
2023-04-15 20:22:05 +0200

    Make program testable

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use std::env;
 use std::str::FromStr;
 
 fn main() {
-    let counter_str = env::args().nth(1).unwrap();
-    let counter = i32::from_str(&counter_str).unwrap();
+    let args = std::env::args().collect();
+    count(args)
+}
 
-    for i in counter..10 {
-        println!("{i}");
+fn count(args: Vec<String>) {
+    let x = args
+        .get(1)
+        .map(|s: &String| i32::from_str(s))
+        .unwrap_or(Ok(0))
+        .unwrap_or_else(|err| {
+            println!("Error while parsing: {}", err);
+            0
+        });
+
+    for i in x..10 {
+        println!("{i}")
     }
 }

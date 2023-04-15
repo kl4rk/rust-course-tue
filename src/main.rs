d643237aa8c83e673b7bdbb3caaa1b88f276a6c5
kl4rk <w.lickefett@googlemail.com>
2023-04-15 19:11:07 +0200

    Read start counter from cli args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::env;
+use std::str::FromStr;
+
 fn main() {
-    let mut counter = 0;
+    let counter_str = env::args().nth(1).unwrap();
+    let counter = i32::from_str(&counter_str).unwrap();
 
     for i in counter..10 {
         println!("{i}");
     }
-
-    println!("Fetttig")
 }

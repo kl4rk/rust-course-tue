28f44881cf9dbee950e8f4ee892329645d460178
kl4rk <w.lickefett@googlemail.com>
2023-04-15 18:06:32 +0200

    Implement first milestone

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let mut print_out = 10;
+    let mut counter = 0;
+
+    loop {
+        println!("{print_out}");
+        print_out += 1;
+        if counter == 10 {
+            break
+        } else {
+            counter += 1;
+        }
+    }
+
+    println!("Fetttig")
 }

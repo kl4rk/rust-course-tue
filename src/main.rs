5ca73013e18646e4f4fca9bf3d4f2016688442b3
kl4rk <w.lickefett@googlemail.com>
2023-04-15 20:31:20 +0200

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -20,14 +20,19 @@ fn count(args: Vec<String>) {
     }
 }
 
-#[test]
-fn test_my_program() {
-    let test_vec = vec![String::from("foo"), String::from("1")];
-    count(test_vec)
-}
+#[cfg(test)]
+mod test {
 
-#[test]
-fn test_my_program_42() {
-    let test_vec = vec![String::from("foo"), String::from("42")];
-    count(test_vec)
+    use super::count;
+    #[test]
+    fn test_my_program() {
+        let test_vec = vec![String::from("foo"), String::from("1")];
+        count(test_vec)
+    }
+
+    #[test]
+    fn test_my_program_42() {
+        let test_vec = vec![String::from("foo"), String::from("42")];
+        count(test_vec)
+    }
 }

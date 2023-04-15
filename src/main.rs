117f76c0be41611016a4d6747bf847f05903ebaa
kl4rk <w.lickefett@googlemail.com>
2023-04-15 20:29:25 +0200

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -19,3 +19,15 @@ fn count(args: Vec<String>) {
         println!("{i}")
     }
 }
+
+#[test]
+fn test_my_program() {
+    let test_vec = vec![String::from("foo"), String::from("1")];
+    count(test_vec)
+}
+
+#[test]
+fn test_my_program_42() {
+    let test_vec = vec![String::from("foo"), String::from("42")];
+    count(test_vec)
+}
